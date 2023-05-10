737e0b403e6f5ac526bc1a24e80d466c719d4d05
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-10 14:43:31 -0300

    Atualização do arquivo da aula de if

---
+++ Python - Iniciante/Aula 03/If.py
@@ -0,0 +1,22 @@
+numero = 1
+if numero ==1:
+    print('numero é igual a 1')
+
+if(numero ==2):
+    print('numero é igual a 2')
+
+numero=2
+
+if numero == 1:
+    print("numero é igual a 1")
+else:
+    print("numero é diferente de 1")
+
+
+nome = "Fernando"
+if("z" in nome):
+    print("O nome tem a letra z")
+elif ("F" in nome):
+    print("O nome tem a letra F")
+else:
+    pass
