e8cf5fdb5f8d1aeea3e544aee28edd1b1d2c2eff
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-10 14:48:09 -0300

    Criaçao e atualizacao do arquivo da aula de For

---
+++ Python - Iniciante/Aula 03/for.py
@@ -0,0 +1,10 @@
+for x in range(0,5):
+    print("O valor de x é: ",x)
+
+nome = "Fernando"
+for letra in nome:
+    print(letra)
+
+lista = ["Fernando",30,"Palmeiras",40,"Tibia",820]
+for item in lista:
+    print(item)
