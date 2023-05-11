ed76dfdbbe2819be5b64de28c83975d95230693f
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-10 21:43:51 -0300

    Criação do arquivo da aula 3  e atualização - Funçoes das strings dentro da função print()

---
+++ Python basico - avançado/Secao_3_parte_1/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Fernando Henrique')
+print(1, 'Fernando "Henrique"')
+
+# Aspas duplas
+print("Fernando Henrique")
+print(2, "Fernando 'Henrique'")
+
+# Escape
+print("Fernando \"Henrique\"")
+
+# r
+print(r"Fernando \"Henrique\"")
