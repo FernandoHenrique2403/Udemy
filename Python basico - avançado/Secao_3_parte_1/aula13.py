6946d8fe1b4b44b5a97df0f1a3e79a70b16fa76b
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-11 17:07:44 -0300

    Criação do arquivo da aula 13 - f Strings

---
+++ Python basico - avançado/Secao_3_parte_1/aula13.py
@@ -0,0 +1,8 @@
+nome ='Fernando Henrique'
+altura = 1.71
+peso = 110
+imc = peso / (altura **2)
+linha_1 = f'{nome} tem {altura:,.2f} de altura'
+linha_2= f'pesa {peso} quilos e seu imc é: '
+linha_3 = f'{imc:,.2f}'
+print(linha_1,'\n',linha_2,'\n',linha_3)
