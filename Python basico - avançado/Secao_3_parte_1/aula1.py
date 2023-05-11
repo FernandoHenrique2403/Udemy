b867043f46562ec11032dfcaf42472db6f3c9cc5
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-10 21:16:21 -0300

    Atualização do arquivo da aula 01 - Comentários com # e docStrings

---
+++ Python basico - avançado/Secao_3_parte_1/aula1.py
@@ -0,0 +1,12 @@
+"""
+    isso se chama docString, funciona como um comentário 
+"""
+'''
+    isso também é um docString, funciona igualmente ao de cima
+'''
+# "#" funcionam como comentarios para os códigos em python
+
+
+print(123)
+
+print(456)
