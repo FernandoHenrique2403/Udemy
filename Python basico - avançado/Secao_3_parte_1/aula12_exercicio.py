8acf6ee109b8a11aa524516e5da5661831200964
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-11 16:43:55 -0300

    Criação do exercicio - imc e squencia de sinais

---
+++ Python basico - avançado/Secao_3_parte_1/aula12_exercicio.py
@@ -0,0 +1,11 @@
+nome ='Fernando Henrique'
+altura = 1.71
+peso = 110
+imc = peso / (altura * altura)
+print(imc)
+
+nome ='Fernando Henrique'
+altura = 1.71
+peso = 110
+imc = peso / (altura **2)
+print(imc)
