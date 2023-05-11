5cb39cff1746bffd92d4803350e2ed68c77d0553
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-11 16:08:48 -0300

    Criação e atualizacao da aula 7- variaveis

---
+++ Python basico - avançado/Secao_3_parte_1/aula7.py
@@ -0,0 +1,10 @@
+nome_completo = "Fernando Henrique dos Santos"
+soma_dois_mais_dois = 2+2
+int_um = int('1')
+print(nome_completo,soma_dois_mais_dois)
+print(int_um,type(int_um))
+
+nome = 'Fernando'
+idade = 30
+maior_de_idade = idade>=18
+print('nome:',nome)
