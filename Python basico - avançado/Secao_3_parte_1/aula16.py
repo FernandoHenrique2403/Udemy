be9e3560ca6a1ea603e4c2a66dc69bfef337f76a
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-12 15:17:45 -0300

    Criação e atualização dos arquivos da aula 17 - Inputs com if elif e else

---
+++ Python basico - avançado/Secao_3_parte_1/aula16.py
@@ -0,0 +1,8 @@
+entrada =input("Voce quer entrar ou sair ? ")
+
+if (entrada == "entrar"):
+    print("Entrou")
+elif(entrada == 'sair'):
+    print("voce saiu do sistema ")
+else:
+    print('Informação invalida')
