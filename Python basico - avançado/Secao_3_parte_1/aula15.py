5015e612de81726f3dd539f0f5370db656240b7d
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-12 14:13:51 -0300

    Atualizacao do arquivo da aula 15 - input de usuarios e suas primeiras conversoes

---
+++ Python basico - avançado/Secao_3_parte_1/aula15.py
@@ -0,0 +1,9 @@
+nome = input ('qual seu nome? ')
+print(f'O seu nome é {nome}')
+
+numero_1 = input("Digite um numero: ")
+numero_2 = input("Digite outro numero: ")
+int_numero1= int(numero_1)
+int_numero2= int(numero_2)
+print(f'a soma dos números é: {int_numero1+int_numero2}')
+
