106c364d6da84fd3ad51873697646310120b489c
FernandoHenrique2403 <fhsantos.dev@gmail.com>
2023-05-24 12:55:58 -0300

    Criação do arquivo da aula 25 - Exercicios

---
+++ Python basico - avançado/testes.py
@@ -0,0 +1,11 @@
+variavel_a = 1 or 0
+variavel_b = 0 or 1
+print(variavel_a, variavel_a)
+
+nome = 'Maria Carmo'
+ 
+if ' ' in nome:
+    print(f'O nome {nome} tem espaços.')
+else:
+    print(f'O nome {nome} NÃO tem espaços.')
+ 
